cfa595f5fa2f36cfb59eb08c438ce5f12563925e
Elivelton Carneiro <eliveltonwess@gmail.com>
2024-07-24 23:25:14 -0300

    novas atualizaçoes

---
+++ src/components/GroupCard/index.tsx
@@ -0,0 +1,20 @@
+import {Container, Title, Icon} from './styles';
+
+
+type Props = {
+    title: string;
+}
+
+
+export function GroupCard({title} :  Props){
+    return (
+        <Container >
+           <Icon />
+           <Title>
+            
+            {title}
+            
+            </Title>
+        </Container>
+    )
+}
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,17 @@
-
-
 import { Header } from '@components/Header';
 import{ Container,} from './styles';
+import { Highlight } from '@components/HighLight';
+import { GroupCard } from '@components/GroupCard';
 
 export function Groups() {
   return (
     <Container>
       <Header  />
+      <Highlight 
+        title='Turmas'
+        subtitle='Jogue com a sua turma'
+      />
+      <GroupCard title='Galera da Desgraça'/>
     </Container>
   );
 }
